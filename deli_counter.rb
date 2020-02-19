0385fa41c8ca7d727182b160734b4866daaecb2e
Brian Vehige <brianvehige@gmail.com>
2020-02-19 05:54:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(x)
+  line_array = []
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0 
+    katz_deli.each.with_index do |name, index| 
+     puts "The line is currently: #{index + 1}. #{name} "
+   end
+ end
+ end
+ 
+ 
+ 
+ 
+    
+  
