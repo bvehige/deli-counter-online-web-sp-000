7858fddc2004abd629638b5a6ff6fd129c6bd3cc
Brian Vehige <brianvehige@gmail.com>
2020-02-19 07:02:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,9 @@ def line(x)
    end
  end
  
- def take_a_number(katz_deli, name)
-   katz_deli.push(name)
-   puts "Welcome, #{name}.  You are number #{katz_deli.length} in line."
+ def take_a_number(katz_deli, new_client)
+   katz_deli.push(new_client)
+   puts "Welcome, #{new_client}.  You are number #{katz_deli.length} in line."
   end
   
  def now_serving(katz_deli)
